cec40d22574ce9c1daf0a8b3b99021d589dd7ca3
Ronggur Mahendra Widya Putra <ronggur.mwp@gmail.com>
2022-05-23 18:59:37 +0700

    modifying video to images

--- videoToImages.py
+++ videoToImages.py
@@ -1,24 +1,13 @@
-# import cv2
-# # vidcap = cv2.VideoCapture('./Data/Video/Test.mp4')
-# success,image = vidcap.read()
-# count = 0
-# success = True
-# while success:
-#   success,image = vidcap.read()
-#   cv2.imwrite("./Data/Images/frame%d.jpg" % count, image)     # save frame as JPEG file
-#   if cv2.waitKey(10) == 27:                     # exit if Escape is hit
-#       break
-#   count += 1
-
-
 import cv2
-outDir = "./Data/Images/frame%d.jpg"
+outDir = "./Data/Images/%d.jpg"
+
+videoFile = input('Enter Video File Name (file akan di write ke directory ./Data/Images): ')
 videoFile ='./Data/Video/Test.mp4'
-vidcap = cv2.VideoCapture('./Data/Video/Test.mp4')
+vidcap = cv2.VideoCapture(videoFile)
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("./Data/Images/frame%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read and Save frame %d: '% count, success)
+  cv2.imwrite(outDir % count, image)     # save frame sebagai JPEG file      
+  success,image = vidcap.read() # kalau fail berarti bisa masalah file/ tidak ada frame lagi
+  print('Read and Save frame %d: '% count, success) 
   count += 1
